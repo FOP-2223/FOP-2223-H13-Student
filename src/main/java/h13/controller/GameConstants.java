a0aafdcdca774e3dea83e5e61ae44ed9508ded5a
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2023-01-30 11:53:17 +0100

    Fix typo in constant name

--- src/main/java/h13/controller/GameConstants.java
+++ src/main/java/h13/controller/GameConstants.java
@@ -52,7 +52,7 @@ public final class GameConstants {
     /**
      * The relative Amount of padding between the enemies.
      */
-    public static final double SHIP_PADING = 0.01;
+    public static final double SHIP_PADDING = 0.01;
 
     /**
      * The relative horizontal movement distance.
@@ -77,7 +77,7 @@ public final class GameConstants {
     /**
      * The relative ship width.
      */
-    public static final double RELATIVE_SHIP_SIZE = (1d - HORIZONTAL_ENEMY_MOVE_DISTANCE) / ENEMY_COLS - 2 * SHIP_PADING;
+    public static final double RELATIVE_SHIP_SIZE = (1d - HORIZONTAL_ENEMY_MOVE_DISTANCE) / ENEMY_COLS - 2 * SHIP_PADDING;
 
     /**
      * The ship width.
